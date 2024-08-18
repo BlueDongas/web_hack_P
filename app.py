d4cc654f676b2385c64895c59467bd66880fa7c3
BlueDongas <web3570@gmail.com>
2024-08-18 20:20:05 +0900

    db connect change

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import pymysql
 
 app = Flask(__name__)
 
@@ -14,14 +15,21 @@ def Reflected_result():
 
 @app.route("/Restored_result",methods=['POST'])
 def Restored_result():
+
     result = request.form.get('Script_Input2')
-    f=open('content.txt','a')
-    f.write(result)
-    f.write("\n")
-    f.close()
-    content = open('./content.txt','r').read()
+    db = pymysql.connect(host='localhost', user='root',passwd='1234',db='webhack',charset='utf8')
+    cur = db.cursor()
+    write_sql = "INSERT INTO board (name, content) VALUES(%s,%s)"
+    values = ("public",result)
+    cur.execute(write_sql,values)
+    db.commit()
     
-    return render_template('Restored_result.html',content=content)
+    load_sql = "SELECT * from board"
+    cur.execute(load_sql)
+
+    data_list = cur.fetchall()
+    
+    return render_template('Restored_result.html',data_list=data_list)
 
 #@app.route("/Delete",methods=['POST'])
 #def Delete_file():
